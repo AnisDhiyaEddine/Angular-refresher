e839262cbea7ddda856a2c9f8c12cd5076b3a954
ANISDHIYAEDDINE <dhiaeboudiaf@gmail.com>
2021-06-19 21:07:16 +0200

    logic added

--- pw/src/app/app.component.ts
+++ pw/src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ],
 })
 export class AppComponent {
-  title = 'pw';
+	password: any = '';
+	length = 0;
+	useLetters = false;
+	useNumbers = false;
+	useSymbols = false;
+
+	onLengthChange(event: any) {
+		if (!isNaN(parseInt(event.target.value))) {
+			this.length = parseInt(event.target.value);
+		}
+	}
+	onUseLetterChange() {
+		this.useLetters = !this.useLetters;
+	}
+	onUseNumbersChange() {
+		this.useNumbers = !this.useNumbers;
+	}
+	onUseSymbolsChange() {
+		this.useSymbols = !this.useSymbols;
+	}
+
+	onButtonClick() {
+		let valid: string | Array<string> = '';
+		let letters = 'azertyuiopmlkjhgfdsqwxcvbn';
+		let numbers = '1234567890';
+		let chars = "&²é'(-è_çà_çà)=@#{[|``  ^@]}";
+		valid = this.useLetters ? valid + letters : valid;
+		valid = this.useNumbers ? valid + numbers : valid;
+		valid = this.useSymbols ? valid + chars : valid;
+
+		valid = valid.split('');
+		this.password = '';
+		for (let i = 0; i < this.length; i++) {
+			this.password +=
+				valid[
+					Math.floor(Math.random() * valid.length)
+				];
+		}
+	}
 }
