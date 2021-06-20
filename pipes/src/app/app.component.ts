35ce486e2cdd91d943d4746e156c3b2c8fa5cd72
ANISDHIYAEDDINE <dhiaeboudiaf@gmail.com>
2021-06-20 20:21:37 +0200

    pipes added

---
+++ pipes/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  name: string = "";
+  date: string = "";
+
+  onNameChange(event: any) {
+    this.name = event.target.value;
+  }
+
+    onDateChange(event: any) {
+    this.date = event.target.value;
+  }
+}
