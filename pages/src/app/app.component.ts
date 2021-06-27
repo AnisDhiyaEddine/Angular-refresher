0c1f913f1ec843988a1e50feb47e0944f633c677
ANISDHIYAEDDINE_BOUDIAF <dhiaeboudiaf@gmail.com>
2021-06-27 12:57:14 +0200

    custom directives init

--- pages/src/app/app.component.ts
+++ pages/src/app/app.component.ts
@@ -6,5 +6,88 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+  currentPage = 0;
+  images = [
+    {
+      title: "Beach girl",
+      url: "https://images.unsplash.com/photo-1544963823-6267c70c3e11?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmVhY2glMjBnaXJsfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+    },
+    {
+      title: "Garden house",
+      url:"https://images.unsplash.com/photo-1555667309-35d7a000ec00?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2FyZGVuJTIwaG91c2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+    },{
+      title: "SA",
+      url:"https://images.unsplash.com/photo-1536685868704-82746720c672?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmF5JTIwYXJlYXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+    },{
+      title: "Beach girl",
+      url: "https://images.unsplash.com/photo-1544963823-6267c70c3e11?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmVhY2glMjBnaXJsfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+    },
+    {
+      title: "Garden house",
+      url:"https://images.unsplash.com/photo-1555667309-35d7a000ec00?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2FyZGVuJTIwaG91c2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+    },{
+      title: "SA",
+      url:"https://images.unsplash.com/photo-1536685868704-82746720c672?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmF5JTIwYXJlYXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+    },{
+      title: "Beach girl",
+      url: "https://images.unsplash.com/photo-1544963823-6267c70c3e11?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmVhY2glMjBnaXJsfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+    },
+    {
+      title: "Garden house",
+      url:"https://images.unsplash.com/photo-1555667309-35d7a000ec00?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2FyZGVuJTIwaG91c2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+    },{
+      title: "SA",
+      url:"https://images.unsplash.com/photo-1536685868704-82746720c672?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmF5JTIwYXJlYXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+    },{
+      title: "Beach girl",
+      url: "https://images.unsplash.com/photo-1544963823-6267c70c3e11?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmVhY2glMjBnaXJsfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+    },
+    {
+      title: "Garden house",
+      url:"https://images.unsplash.com/photo-1555667309-35d7a000ec00?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2FyZGVuJTIwaG91c2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+    },{
+      title: "SA",
+      url:"https://images.unsplash.com/photo-1536685868704-82746720c672?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmF5JTIwYXJlYXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+    },{
+      title: "Beach girl",
+      url: "https://images.unsplash.com/photo-1544963823-6267c70c3e11?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmVhY2glMjBnaXJsfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+    },
+    {
+      title: "Garden house",
+      url:"https://images.unsplash.com/photo-1555667309-35d7a000ec00?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2FyZGVuJTIwaG91c2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+    },{
+      title: "SA",
+      url:"https://images.unsplash.com/photo-1536685868704-82746720c672?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmF5JTIwYXJlYXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+    },{
+      title: "Beach girl",
+      url: "https://images.unsplash.com/photo-1544963823-6267c70c3e11?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmVhY2glMjBnaXJsfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+    },
+    {
+      title: "Garden house",
+      url:"https://images.unsplash.com/photo-1555667309-35d7a000ec00?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2FyZGVuJTIwaG91c2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+    },{
+      title: "SA",
+      url:"https://images.unsplash.com/photo-1536685868704-82746720c672?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmF5JTIwYXJlYXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+    },{
+      title: "Beach girl",
+      url: "https://images.unsplash.com/photo-1544963823-6267c70c3e11?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmVhY2glMjBnaXJsfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+    },
+    {
+      title: "Garden house",
+      url:"https://images.unsplash.com/photo-1555667309-35d7a000ec00?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2FyZGVuJTIwaG91c2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+    },{
+      title: "SA",
+      url:"https://images.unsplash.com/photo-1536685868704-82746720c672?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmF5JTIwYXJlYXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+    },{
+      title: "Beach girl",
+      url: "https://images.unsplash.com/photo-1544963823-6267c70c3e11?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YmVhY2glMjBnaXJsfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+    },
+    {
+      title: "Garden house",
+      url:"https://images.unsplash.com/photo-1555667309-35d7a000ec00?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Z2FyZGVuJTIwaG91c2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+    },{
+      title: "SA",
+      url:"https://images.unsplash.com/photo-1536685868704-82746720c672?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8YmF5JTIwYXJlYXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
+    }
+  ]
 }
---
+++ pages/src/app/class.directive.ts
@@ -0,0 +1,13 @@
+/* import { Directive, ElementRef, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appClass]'
+})
+export class ClassDirective {
+  @Input() color: string;
+  constructor( private element : ElementRef) {  
+    setTimeout(()=>     this.element.nativeElement.style.color = this.color, 50)
+  }
+
+}
+ */
