f0c7a1756319efd22369ec04c80076ef8169f252
ANISDHIYAEDDINE_BOUDIAF <dhiaeboudiaf@gmail.com>
2021-07-09 22:23:40 +0200

    wikipedia wrapped up

--- wikipedia/src/app/app.component.ts
+++ wikipedia/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { WikipediaService } from "./wikipedia.service"
+import { HttpClientModule } from "@angular/common/http"
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'wikipedia';
+  pages = [];
+  constructor(private serivce : WikipediaService){
+  }
+  
+  onSubmitted(term: any){
+    this.serivce.search(term).subscribe(( response : any ) => this.pages = response.query.search )
+  }
 }
